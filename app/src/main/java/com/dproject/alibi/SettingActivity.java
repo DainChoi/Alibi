d3a906104c8c2d82757a8b98125f8b24c86927c2
DainChoi <8677k@naver.com>
2021-05-03 00:34:31 +0900

    botttomNavi

--- app/src/main/java/com/dproject/alibi/SettingActivity.java
+++ app/src/main/java/com/dproject/alibi/SettingActivity.java
@@ -10,51 +10,42 @@ import android.widget.ImageButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
-    // private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-/*
+        setContentView(R.layout.activity_setting);
+
         mFirebaseAuth = FirebaseAuth.getInstance();
 
-        Button btn_logout = findViewById(R.id.btn_logout);
+        ImageButton btn_logout = findViewById(R.id.btn_logout);
         btn_logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 로그아웃 하기
                 mFirebaseAuth.signOut();
 
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });*/
-
-        ImageButton btn_setting = findViewById(R.id.btn_setting);
-        btn_setting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(MainActivity.this, SettingActivity.class);
+                Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
 
-        Button btn_move = findViewById(R.id.btn_move);
-        btn_move.setOnClickListener(new View.OnClickListener() {
+        ImageButton btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this, WorkActivity.class);
+                Intent intent = new Intent(SettingActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
+
+
 
         // 탈퇴 처리
         // mFirebaseAuth.getCurrentUser().delete();
