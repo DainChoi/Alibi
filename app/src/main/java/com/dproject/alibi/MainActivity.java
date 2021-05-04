5e3c3e2753c561985e1cc6ff09e8f47a4c79d420
DainChoi <8677k@naver.com>
2021-05-04 14:10:02 +0900

    botttomNavi

--- app/src/main/java/com/dproject/alibi/MainActivity.java
+++ app/src/main/java/com/dproject/alibi/MainActivity.java
@@ -12,27 +12,11 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
-    // private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-/*
-        mFirebaseAuth = FirebaseAuth.getInstance();
-
-        Button btn_logout = findViewById(R.id.btn_logout);
-        btn_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 로그아웃 하기
-                mFirebaseAuth.signOut();
-
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });*/
 
         ImageButton btn_setting = findViewById(R.id.btn_setting);
         btn_setting.setOnClickListener(new View.OnClickListener() {
