57d3b2a4bacadcfdaeca45a4e1afcfa59508acce
DainChoi <8677k@naver.com>
2021-07-01 12:51:36 +0900

    MainActivity recyclerview

--- app/src/main/java/com/dproject/alibi/CustomAdapter.java
+++ app/src/main/java/com/dproject/alibi/CustomAdapter.java
@@ -2,6 +2,7 @@ package com.dproject.alibi;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Build;
 import android.view.LayoutInflater;
@@ -13,10 +14,15 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
@@ -24,8 +30,10 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
     private Context context;
     private Activity activity;
-    private ImageButton btn_modify2;
+    private ImageButton btn_in, btn_out;
     private ArrayList<MyWork> arrayList;
+    private TextView time_in, time_out;
+    DatabaseReference databaseReference;
 
 
     public CustomAdapter(ArrayList<MyWork> arrayList, Context context) {
@@ -38,6 +46,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(context);
         View view = inflater.inflate(R.layout.my_row, parent, false);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Alibi").child("MyWork");
         return new MyViewHolder(view);
     }
 
@@ -59,7 +68,42 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
             }
         });
 
+        holder.mainLayout.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setTitle("근무지 삭제하기");
+                builder.setMessage("해당 근무지를 삭제하시겠습니까?");
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override // ERROR: 해당 항목 삭제 x -> 전체 삭제됨
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(context, "삭제되었습니다.", Toast.LENGTH_SHORT).show();
+                        databaseReference.removeValue(); // 데이터베이스에서 항목 제거 //Error: 전부 삭제
+                        //  databaseReference.child(OwnerWork.workid).removeValue();
+                        deleteItem(position); // 리사이클러뷰 다시 정렬/*
 
+
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                builder.show();
+
+                return true;
+            }
+        });
+
+
+    }
+
+    private void deleteItem(int position) {
+        arrayList.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, arrayList.size());
     }
 
     @Override
@@ -72,7 +116,8 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
         ImageView work_num_txt;
         TextView  work_title_txt, work_id_txt, work_address_txt;
         LinearLayout mainLayout;
-        ImageButton btn_modify2;
+        ImageButton btn_in, btn_out;
+        TextView time_in, time_out;
 
         MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -80,7 +125,11 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
             work_title_txt = itemView.findViewById(R.id.work_title_txt);
             work_id_txt = itemView.findViewById(R.id.work_id_txt);
             work_address_txt = itemView.findViewById(R.id.work_address_txt);
-           // btn_modify2 = itemView.findViewById(R.id.btn_modify2);
+            btn_in = itemView.findViewById(R.id.btn_in);
+            btn_out = itemView.findViewById(R.id.btn_out);
+            time_in = itemView.findViewById(R.id.time_in);
+            time_out = itemView.findViewById(R.id.time_out);
+
             mainLayout = itemView.findViewById(R.id.mainLayout);
             //Animate Recyclerview
             //Animation translate_anim = AnimationUtils.loadAnimation(context, R.anim.translate_anim);
--- app/src/main/java/com/dproject/alibi/MainActivity.java
+++ app/src/main/java/com/dproject/alibi/MainActivity.java
@@ -141,28 +141,6 @@ public class MainActivity extends AppCompatActivity {
     */
 
 
-    void confirmDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Delete All?");
-        builder.setMessage("Are you sure you want to delete all Data?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                MyDatabaseHelper myDB = new MyDatabaseHelper(MainActivity.this);
-                myDB.deleteAllData();
-                //Refresh Activity
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
 
-            }
-        });
-        builder.create().show();
-    }
 
 }
