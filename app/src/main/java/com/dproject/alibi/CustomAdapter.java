a1d8f528a12ce40244922421d3720efcd5ad3a2e
DainChoi <8677k@naver.com>
2021-08-16 09:35:17 +0900

    Revert "logo"

--- app/src/main/java/com/dproject/alibi/CustomAdapter.java
+++ app/src/main/java/com/dproject/alibi/CustomAdapter.java
@@ -70,7 +70,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHold
 
         holder.btn_in.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) { //ERROR
+            public void onClick(View v) {
                 Date currentTime = Calendar.getInstance().getTime();
                 SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm");
                 String output = dateFormat.format(currentTime);
